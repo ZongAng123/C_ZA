f5eed7e44f8172082875fea8436fe7d520bab34d
纵昂 <zongang15@yahoo.com>
2021-08-08 09:03:15 +0800

    Initial Commit

---
+++ C_ZA/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  C_ZA
+//
+//  Created by 纵昂 on 2021/8/8.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
